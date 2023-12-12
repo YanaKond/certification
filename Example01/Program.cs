9c2b4a0c2fdaf9dc8a6278e4f8d9b2c2077beb38
Yana Kond <https://github.com/YanaKond>
2023-12-12 20:17:34 +0300

    add example 1

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,1 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string rec(int M, int N)
+{
+    if (M == N)
+        return $"{N} ";
+    if(M < N)
+        return rec(M, N - 1) + $"{N} ";
+    else
+        return rec(M, N + 1) + $"{N} ";
+}
+
+Console.WriteLine(rec(M, N));
