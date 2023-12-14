81b8bbdfe55a1ed8b38684e4b4dc54542d21f4b3
Yana Kond <https://github.com/YanaKond>
2023-12-14 19:11:27 +0300

    add example 3

---
+++ Example03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[x];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 101);
+}
+Console.WriteLine(string.Join(", ", array));
+
+void RecurseArray(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write($"{array[index]} ");
+        RecurseArray(array, index - 1);
+    }
+}
+
+RecurseArray(array, array.Length - 1);
